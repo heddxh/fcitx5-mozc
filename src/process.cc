aabc542d5b739b9e3bdffc540afbc422951896e5
Qijia Liu <liumeo@pku.edu.cn>
2025-04-26 16:38:35 -0400

    fix mozc build; patch protobuf cmake to drop irrelevant compilers (#7)

--- src/process.cc
+++ src/process.cc
@@ -1,7 +1,7 @@
 #include "base/process.h"
 
 namespace mozc {
-bool Process::OpenBrowser(const std::string &url) {}
-bool Process::SpawnMozcProcess(const std::string &filename,
-                               const std::string &arg, size_t *pid) {}
-}  // namespace mozc
+bool Process::OpenBrowser(zstring_view url) {}
+bool Process::SpawnMozcProcess(zstring_view filename, zstring_view arg,
+                               size_t *pid) {}
+} // namespace mozc
